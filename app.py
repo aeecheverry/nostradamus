7296113eddfc66221fcdf292a01c4e32cfc22fb6
aeecheverry <aeecheverry97@gmail.com>
2024-04-30 11:14:37 -0500

    ProbabilityFixed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 from models.predictor import Predictor
 import pandas as pd
 from flask_cors import CORS 
-from utils.helpers import get_severity_type, calculate_probability
+from utils.helpers import get_severity_type, calculate_day_probability
 import os
 from api import api
 
@@ -18,7 +18,8 @@ def convert_time_to_hour(X):
 model_severity = Predictor('model.pkl', 'preprocessor.pkl')
 model_restdays = Predictor('model_restdays.pkl', 'preprocessor.pkl')
 
-
+with open('probability.pkl', 'rb') as file:
+    probability_dict = pickle.load(file)
 with open('gender_distribution.pkl', 'rb') as f:
     prob_gender = pickle.load(f)
 with open('vehicle_type_distribution.pkl', 'rb') as f:
@@ -50,7 +51,11 @@ def calculate_probability(accident):
     gender_prob = prob_gender.get(gender, 0)
     vehicle_type_prob = prob_vehicle_type.get(vehicle_type, 0)
     age_prob = prob_age.get(age_group, 0)
-    return gender_prob * age_prob * vehicle_type_prob
+
+    day_of_year = pd.to_datetime(accident['timestamp']).dayofyear
+    day_probability = calculate_day_probability(day_of_year, probability_dict)
+
+    return gender_prob * age_prob * vehicle_type_prob * day_probability
 
 #Temporal Severity
 def calculate_severity(restdays):
@@ -64,13 +69,11 @@ def calculate_severity(restdays):
 def process_accident_data(accident):
     """Procesa los datos de un accidente individual."""
     # Extraer y calcular información relevante
-    accident_probability = calculate_probability(accident)
-
     if 'time' not in accident:
         accident['time'] = pd.to_datetime(accident['timestamp']).time().strftime('%H:%M:%S')
 
     accident.pop('timestamp', None)
-    return accident, accident_probability
+    return accident
 
 def make_prediction(accident):
     """Realiza predicciones basadas en los datos del accidente."""
@@ -88,14 +91,17 @@ def predict_endpoint():
         data = [data]
 
     for accident in data:
-        processed_accident, accident_probability = process_accident_data(accident)
-        severity, restdays, severity_type = make_prediction(processed_accident)
+        probability = calculate_probability(accident)
+        processed_accident = process_accident_data(accident)
+        severity, restdays, _ = make_prediction(processed_accident)
         severity = calculate_severity(restdays)
+        severity_type = get_severity_type(severity)
+
         response = {
             'id': accident['id'],
             'severity': severity,
             'restdays': restdays,
-            'probability': accident_probability,
+            'probability': probability,
             'type': severity_type
         }
         responses.append(response)
